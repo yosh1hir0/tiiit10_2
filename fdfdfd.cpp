d5906e1e6a71bcc1a65ddb06a1633bc26e9d9941
Кохнюк Г. Э. <Glebkahnyuk@outlook.com>
2025-12-05 22:14:02 +0300

    решение

---
+++ fdfdfd.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {64, 34, 34, 25, 61, 12, 22, 11, 90, 42, 99};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    cout << "Original: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    // Bubble sort
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    
+    cout << "Output: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
